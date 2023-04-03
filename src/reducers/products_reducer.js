64f380d651bf598111536fc0992ac8047029bae9
Neto Sena <neetosena@gmail.com>
2023-04-03 20:35:42 +0100

    initial commit

---
+++ src/reducers/products_reducer.js
@@ -0,0 +1,72 @@
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+  GET_SINGLE_PRODUCT_BEGIN,
+  GET_SINGLE_PRODUCT_SUCCESS,
+  GET_SINGLE_PRODUCT_ERROR,
+} from "../actions";
+
+const products_reducer = (state, action) => {
+  if (action.type === SIDEBAR_OPEN) {
+    return { ...state, isSidebarOpen: true };
+  }
+
+  if (action.type === SIDEBAR_CLOSE) {
+    console.log(state);
+    return { ...state, isSidebarOpen: false };
+  }
+
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_product = action.payload.filter(
+      (product) => product.featured === true
+    );
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_product,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return {
+      ...state,
+      products_loading: false,
+      products_error: true,
+    };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product: action.payload,
+    };
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state,
+      single_product_error: true,
+      single_product_loading: false,
+    };
+  }
+
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default products_reducer;
